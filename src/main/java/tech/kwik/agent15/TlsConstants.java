c4c814b37075cb0f450e2ec0539f4e5bd578db51
Peter Doornbosch <peter.doornbosch@luminis.eu>
2026-08-21 10:09:50 +0200

    add group names for Post-Quantum Traditional (PQ/T) hybrid key agreement mechanisms (RFC 10024)

--- src/main/java/tech/kwik/agent15/TlsConstants.java
+++ src/main/java/tech/kwik/agent15/TlsConstants.java
@@ -96,13 +96,18 @@ public class TlsConstants {
     
     public enum NamedGroup {
 
-          /* Elliptic Curve Groups (ECDHE) */
-          secp256r1(0x0017), secp384r1(0x0018), secp521r1(0x0019),
-          x25519(0x001D), x448(0x001E),
+        /* Elliptic Curve Groups (ECDHE) */
+        secp256r1(0x0017), secp384r1(0x0018), secp521r1(0x0019),
+        x25519(0x001D), x448(0x001E),
 
-          /* Finite Field Groups (DHE) */
-          ffdhe2048(0x0100), ffdhe3072(0x0101), ffdhe4096(0x0102),
-          ffdhe6144(0x0103), ffdhe8192(0x0104),
+        /* Finite Field Groups (DHE) */
+        ffdhe2048(0x0100), ffdhe3072(0x0101), ffdhe4096(0x0102),
+        ffdhe6144(0x0103), ffdhe8192(0x0104),
+
+        /* https://www.rfc-editor.org/info/rfc10024/#section-7 */
+        X25519MLKEM768(0x11ec),
+        SecP256r1MLKEM768(0x11eb),
+        SecP384r1MLKEM1024(0x11ed)
         ;
 
         public short value;
